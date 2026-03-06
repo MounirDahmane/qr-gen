e7afdd159d9582b7322526f2aa56e8431f975935
Dahmane Mounir <dahmanemounirbaligh@gmail.com>
2026-03-07 00:58:00 +0100

    performing a qr code generation, using qrcode and image crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use image::Luma;
+use qrcode::QrCode;
+
+fn generate(data: &str) {
+    // Encode some data into bits.
+    let code = QrCode::new(data.as_bytes()).unwrap();
+
+    // Render the bits into an image.
+    let img = code.render::<Luma<u8>>().build();
+
+    // Save the image.
+    img.save("./qrcode.png").unwrap();
+}
+
+
+fn main(){
+    generate("https://github.com/MounirDahmane");
 }
